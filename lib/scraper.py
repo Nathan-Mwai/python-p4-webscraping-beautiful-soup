52d498cee928f1165e33d6ca8fec385edf08fb47
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-10-05 07:16:47 +0300

    finishes lab

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,11 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+# print(doc.select('.heading-primary')[0].contents)
+
+courses = doc.select('.heading-25-extrabold.color-black.mb-none')
+
+for course in courses:
+    print(course.content[0].strip())
+print(courses)
